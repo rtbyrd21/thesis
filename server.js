37fb0e855f599cda194c8aa26771b5d390d2de3e
Robert Byrd <robert.byrd2@kroger.com>
2015-09-09 23:44:46 -0400

    reinit

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express  = require('express');
 var app      = express();
-var argv     = require('yargs').argv; if(!argv._.length){ argv._ = 'mongo'; };
 var env      = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config   = require('./server/config')[env];
-var dbConfig = require('./server/routes/index.js')[argv._];
+var dbConfig = require('./server/routes/index.js')['mongo'];
 
 
 require('./server/express.js')(app, config);
 
-dbConfig.connect(config);
+// dbConfig.connect(config);
 require(dbConfig.routes)(app);
 
 app.listen(config.port);	
